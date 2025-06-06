e9fba9f94b1d46c2825398088468e227eddffc88
Besson <yanic.besson@gmail.com>
2025-06-06 12:30:31 +0200

    removed dist and node modules

--- dist/camera.d.ts
+++
@@ -1,7 +0,0 @@
-import * as THREE from 'three';
-export declare const camera: THREE.PerspectiveCamera;
-export declare function initCamera(): void;
-export declare const startQuaternion: THREE.Quaternion;
-export declare const endQuaternion: THREE.Quaternion;
-export declare function getCameraLookAtPoint(): THREE.Vector3;
-export declare function setupCameraResize(): void;
