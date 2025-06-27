0a353479621e9e0ef402426548f70a44e9780c0b
winno-GmbH <accounts@winno.ch>
2025-06-27 15:06:13 +0200

    push

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,3 @@
-import { CAMERA_POSITIONS } from "../paths/pathpoints";
-
 export const isMobile = window.innerWidth < 768;
 
 export const CAMERA_CONFIG = {
@@ -13,13 +11,3 @@ export const ASSETS = {
   mazeTexture: "https://c-am.b-cdn.net/CAM/matcap24.png",
   mazeModel: "https://c-am.b-cdn.net/CAM/c-am-assets-3.glb",
 };
-
-export const startPosition = isMobile
-  ? CAMERA_POSITIONS.startMobile
-  : CAMERA_POSITIONS.startDesktop;
-export const secondPosition = isMobile
-  ? CAMERA_POSITIONS.secondMobile
-  : CAMERA_POSITIONS.secondDesktop;
-export const lookAtPosition = isMobile
-  ? CAMERA_POSITIONS.mobileLookAt
-  : CAMERA_POSITIONS.desktopLookAt;
