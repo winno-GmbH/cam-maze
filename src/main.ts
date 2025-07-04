201b3fcd22be20af3a799f3eb6d20d18b58c8cd5
winno-GmbH <accounts@winno.ch>
2025-07-04 14:33:24 +0200

    push

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,23 @@
 import {
-  setupScene,
   startRenderLoop,
   onFrame,
   initRenderer,
   setupLighting,
+  scene,
 } from "./core/scene";
 import { updateHomeLoop } from "./animation/HomeLoop";
 import { initHomeScrollAnimation } from "./animation/HomeScroll";
 import { loadModel } from "./core/objects";
-import { setupCameraResize } from "./core/camera";
 
 async function main() {
   initRenderer();
   setupLighting();
-  await setupScene();
+  await loadModel(scene);
 
-  // Register frame callbacks
   onFrame(updateHomeLoop);
 
-  // Initialize home scroll animation
   initHomeScrollAnimation();
 
-  // Start the render loop
   startRenderLoop();
 }
 
