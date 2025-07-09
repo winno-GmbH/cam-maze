529dbd82ade48954a361f9cc51935dd54befe590
winno-GmbH <accounts@winno.ch>
2025-07-09 17:18:57 +0200

    push

--- src/core/camera.ts
+++ src/core/camera.ts
@@ -18,25 +18,3 @@ export function setupCamera(): void {
   camera.position.set(startPosition.x, startPosition.y, startPosition.z);
   camera.lookAt(lookAtPosition.x, lookAtPosition.y, lookAtPosition.z);
 }
-
-export const startQuaternion = camera.quaternion.clone();
-export const endQuaternion = new THREE.Quaternion().setFromEuler(
-  new THREE.Euler(-1.5708, 0, 0)
-);
-
-export function getCameraLookAtPoint(): THREE.Vector3 {
-  const direction = new THREE.Vector3(0, 0, -1);
-  direction.applyQuaternion(camera.quaternion);
-  const lookAtPoint = new THREE.Vector3();
-  lookAtPoint.copy(camera.position).add(direction.multiplyScalar(10));
-  return lookAtPoint;
-}
-
-export function setupCameraResize(): void {
-  const updateCamera = () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-  };
-
-  window.addEventListener("resize", updateCamera);
-}
