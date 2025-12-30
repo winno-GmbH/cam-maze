7520829950aa7b9279a2685f0d0098aeb10859a8
winno-GmbH <accounts@winno.ch>
2025-12-30 07:57:46 +0100

    push

--- src/paths/pathpoints.ts
+++ src/paths/pathpoints.ts
@@ -1111,7 +1111,7 @@ export function createHomeScrollPathPoints(
   Object.entries(pausedPositions).forEach(([key, pausedPos]) => {
     const arcPoint = new THREE.Vector3(
       pausedPos.x * (1 / 4) + objectHomeScrollEndPathPoint.x * (3 / 4),
-      1.5,
+      3,
       pausedPos.z * (1 / 4) + objectHomeScrollEndPathPoint.z * (3 / 4)
     );
 
